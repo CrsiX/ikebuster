1700643e9ddc1efa8a49de550628b799dcd5835d
Chris <chris@hopfen.space>
2025-09-08 14:54:12 +0200

    Enable Path MTU Discovery by retrying failed packets

--- ikebuster/src/v2_utils/sender.rs
+++ ikebuster/src/v2_utils/sender.rs
@@ -1,4 +1,5 @@
 use std::collections::VecDeque;
+use std::io::ErrorKind;
 use std::sync::Arc;
 use std::time::Instant;
 
@@ -6,7 +7,7 @@ use isakmp::v2::definitions;
 use isakmp::v2::definitions::{IKEv2, Payload, Proposal, SecurityAssociation};
 use isakmp::v2::utils::get_random_vec;
 use tokio::net::UdpSocket;
-use tracing::instrument;
+use tracing::{debug, error, instrument, warn};
 
 use crate::v2_utils::{Open, ScanOptionsV2, Statistics};
 use crate::ScanError;
@@ -61,10 +62,35 @@ pub(crate) async fn send_packet(
 ) -> Result<(), ScanError> {
     let serialized_msg = packet.try_build().map_err(ScanError::GeneratorFailed)?;
     let ts = Instant::now();
-    let sent_bytes = socket
-        .send(serialized_msg.as_slice())
-        .await
-        .map_err(ScanError::Send)?;
+    let sent_bytes = match socket.send(serialized_msg.as_slice()).await {
+        Ok(v) => v,
+        Err(err) => {
+            warn!("Sending failed: {}: {:#?}", err.kind(), err);
+            // For ErrorKind::Uncategorized errors with error number 90, the MTU
+            // along the path was lower than expected. If Path MTU Discovery is enabled,
+            // this error signals that the sender should reduce the packet size.
+            // However, since the kernel performs Path MTU Discovery as well,
+            // simply retrying to send the packet once will already solve the problem,
+            // as the kernel will fragment the outgoing UDP packet correctly.
+            if let Some(e) = err.raw_os_error() {
+                if e == 90 {
+                    debug!(
+                        "Detected raw OS error value 90. This is likely due to Path "
+                        "MTU Discovery. Retrying to send the packet once..."
+                    );
+                    let sent_bytes = socket.send(serialized_msg.as_slice()).await.map_err(|e| {
+                        error!("Failed to resend the packet after MTU discovery: {}", e);
+                        ScanError::Send(e)
+                    })?;
+                    open.sent.push((packet, ts));
+                    stats.sent_bytes += sent_bytes as u64;
+                    stats.sent_packets += 1;
+                    return Ok(());
+                }
+            }
+            return Err(ScanError::Send(err));
+        }
+    };
     open.sent.push((packet, ts));
     stats.sent_bytes += sent_bytes as u64;
     stats.sent_packets += 1;
